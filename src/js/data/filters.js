7ff2186b1327f12f14d9fac8281b6e0233989f21
demikhovr <demikhovr@gmail.com>
2019-03-03 17:08:20 +0300

    remove moment.js, add data structures for trip-points

--- src/js/data/filters.js
+++ src/js/data/filters.js
@@ -1,5 +1,12 @@
 export default [
-  {name: `everything`, checked: true},
-  {name: `future`},
-  {name: `past`},
+  {
+    name: `everything`,
+    checked: true,
+  },
+  {
+    name: `future`,
+  },
+  {
+    name: `past`,
+  },
 ];
--- src/js/utils/util.js
+++ src/js/utils/util.js
@@ -2,3 +2,15 @@ export const getRandomNumber = (max = 1, min = 0, accuracy = 1) => {
   const randomNumber = Math.random() * (max - min + 1) + (min - 0.5);
   return Math.round(randomNumber * accuracy) / accuracy;
 };
+
+export const getRandomArrayItems = (array, max, min = 0) => {
+  const randomItems = new Set();
+  const amount = getRandomNumber(max, min);
+
+  while (randomItems.size < amount) {
+    const item = array[getRandomNumber(array.length - 1)];
+    randomItems.add(item);
+  }
+
+  return [...randomItems];
+};
